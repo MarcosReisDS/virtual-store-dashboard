27667fbf48bc72cf846dd0dfd019e0a620fe5980
marcos reis <marcos.reis349@gmail.com>
2024-02-27 22:51:45 -0300

    Default setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+import SideBar from './shared/components/SideBar'
+import Router from './shared/router'
 
 interface IApp { }
 const App: FC<IApp> = () => {
+  const [sidebarValue, setSidebarValue] = useState<any>(null);
 
+  // Função para atualizar o valor da SideBar
+  const handleSidebarChange = (value: any) => {
+    setSidebarValue(value);
+  };
   return (
-    <div>
-      Olá mundo!
-    </div>
+    <>
+      <SideBar onSidebarChange={handleSidebarChange}/>
+      <Router sidebarValue={sidebarValue}/>
+    </>
   )
 }
 
