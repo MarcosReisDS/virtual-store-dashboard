d0c1105bd9c923a41e1c65979b9e33b91e85d537
marcos reis <marcos.reis349@gmail.com>
2024-03-28 17:44:45 -0300

    added libs: 'zod', '@hookform/resolvers', 'react-hook-form' and 'jwt-decode' and some adjustments to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,68 @@
-import { FC, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import SideBar from './shared/components/SideBar'
 import Router from './shared/router'
+import { getCookie } from './shared/utils/cookies';
+import { jwtDecode } from "jwt-decode";
+import userApi from './shared/services/user';
+import Contexts, { IUserType, Jwt } from './shared/contexts';
+import { useLocation } from 'react-router-dom';
 
 interface IApp { }
 const App: FC<IApp> = () => {
-  const [sidebarValue, setSidebarValue] = useState<any>(null);
+  const { pathname } = useLocation()
+  const [valueSidebar, setValueSidebar] = useState<boolean>(false)
 
-  // Função para atualizar o valor da SideBar
-  const handleSidebarChange = (value: any) => {
-    setSidebarValue(value);
-  };
+  const [user, setUser] = useState<IUserType>({
+    profile: '',
+    name: '',
+    surname: '',
+    admin: true
+  })
+  const [users, setUsers] = useState<IUserType[]>([])
+
+  const [token, setToken] = useState<string>("")
+
+  useEffect(() => {
+    setToken(getCookie("token").token);
+
+    if (token) {
+      const jwt: Jwt = jwtDecode(token)
+
+      userApi.listUser(jwt?.email, token).then((data: IUserType) => {
+        setUser({
+          id: data?.id,
+          profile: data?.profile,
+          name: data?.name,
+          admin: data?.admin
+        })
+      })
+    }
+
+    userApi.listUsers(Number(user?.admin), token).then((data: any) => {
+      setUsers(data)
+    })
+  }, [token, pathname])
+
   return (
-    <>
-      <SideBar onSidebarChange={handleSidebarChange}/>
-      <Router sidebarValue={sidebarValue}/>
-    </>
+    <Contexts.Provider value={{
+      token,
+      setToken,
+      user,
+      users,
+      valueSidebar,
+      setUser,
+      setUsers,
+      onSidebarChange(value) {
+        setValueSidebar(value);
+      },
+    }}>
+      {window.location.pathname == "/conectar" ?
+        null
+        :
+        <SideBar />
+      }
+      <Router />
+    </Contexts.Provider>
   )
 }
 
